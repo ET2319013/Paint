812cd0b176d72b3257a277a71aa208be5a10a63b
ET2319013 <2319013@vumk.eu>
2024-12-11 09:56:28 +0200

    +menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,18 +30,24 @@
 		{
 			flowLayoutPanel1 = new FlowLayoutPanel();
 			pictureBox1 = new PictureBox();
+			menuStrip1 = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			openToolStripMenuItem = new ToolStripMenuItem();
+			colorToolStripMenuItem = new ToolStripMenuItem();
+			chooseColorToolStripMenuItem = new ToolStripMenuItem();
 			flowLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			menuStrip1.SuspendLayout();
 			SuspendLayout();
 			// 
 			// flowLayoutPanel1
 			// 
-			flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
 			flowLayoutPanel1.AutoScroll = true;
 			flowLayoutPanel1.Controls.Add(pictureBox1);
-			flowLayoutPanel1.Location = new Point(0, 0);
+			flowLayoutPanel1.Dock = DockStyle.Fill;
+			flowLayoutPanel1.Location = new Point(0, 28);
 			flowLayoutPanel1.Name = "flowLayoutPanel1";
-			flowLayoutPanel1.Size = new Size(674, 450);
+			flowLayoutPanel1.Size = new Size(800, 422);
 			flowLayoutPanel1.TabIndex = 0;
 			// 
 			// pictureBox1
@@ -54,23 +60,69 @@
 			pictureBox1.TabStop = false;
 			pictureBox1.Click += pictureBox1_Click;
 			// 
+			// menuStrip1
+			// 
+			menuStrip1.ImageScalingSize = new Size(20, 20);
+			menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, colorToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(800, 28);
+			menuStrip1.TabIndex = 1;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem });
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(46, 24);
+			fileToolStripMenuItem.Text = "File";
+			// 
+			// openToolStripMenuItem
+			// 
+			openToolStripMenuItem.Name = "openToolStripMenuItem";
+			openToolStripMenuItem.Size = new Size(224, 26);
+			openToolStripMenuItem.Text = "Open";
+			// 
+			// colorToolStripMenuItem
+			// 
+			colorToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { chooseColorToolStripMenuItem });
+			colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+			colorToolStripMenuItem.Size = new Size(59, 24);
+			colorToolStripMenuItem.Text = "Color";
+			// 
+			// chooseColorToolStripMenuItem
+			// 
+			chooseColorToolStripMenuItem.Name = "chooseColorToolStripMenuItem";
+			chooseColorToolStripMenuItem.Size = new Size(224, 26);
+			chooseColorToolStripMenuItem.Text = "Choose color";
+			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
 			Controls.Add(flowLayoutPanel1);
+			Controls.Add(menuStrip1);
+			MainMenuStrip = menuStrip1;
 			Name = "Form1";
 			Text = "Form1";
 			flowLayoutPanel1.ResumeLayout(false);
 			flowLayoutPanel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
 
 		private FlowLayoutPanel flowLayoutPanel1;
 		private PictureBox pictureBox1;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem openToolStripMenuItem;
+		private ToolStripMenuItem colorToolStripMenuItem;
+		private ToolStripMenuItem chooseColorToolStripMenuItem;
 	}
 }
